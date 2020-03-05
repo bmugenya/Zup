9f9bbeca8a9482abc072f15afe979042e2c22e97
bmugenya <bmugenya26@gmail.com>
2020-03-05 22:35:40 +0300

    [FEATURE #171654981] implemented the routes specified in README

---
+++ run.py
@@ -0,0 +1,53 @@
+from flask import Flask
+app = Flask(__name__)
+
+# HOME PAGE
+@app.route('/api/v1')
+def index():
+    return 'Index page'
+
+# DASHBOARD
+@app.route('/api/v1/dashboard')
+def dashboard():
+    return 'Dashboard page'
+
+# REGISTER
+@app.route('/api/v1/adduser')
+def addUsers():
+    return 'Register page'
+
+# LOGIN
+@app.route('/api/v1/user')
+def users():
+    return 'Login page'
+
+# CONFIGURATION
+@app.route('/api/v1/config')
+def config():
+    return 'Configuraion page'
+
+# MAKE ANALYSIS
+@app.route('/api/v1/analysis')
+def analysis():
+    return 'add analysis page'
+
+# VIEW ANALYSIS
+@app.route('/api/v1/details')
+def details():
+    return 'view analysis page'
+
+# VIEW PAST ANLYSIS
+@app.route('/api/v1/history')
+def history():
+    return 'Analysis history page'
+
+#VIEW ALL REGISTERED USERS
+@app.route('/api/v1/report')
+def report():
+    return 'List of available users page'
+
+
+
+if __name__ == '__main__':
+    app.debug=True
+    app.run(host = '0.0.0.0', port=5000)
