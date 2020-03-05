9db556b9528c94bb63a8102a62461502764d6ef8
bmugenya <bmugenya26@gmail.com>
2020-03-05 21:51:54 +0300

    [CHORE #171653663] started react project

---
+++ run.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def helloworld():
+    return 'ANU PROGRAMMING COMPETITION'
+
+if __name__ == '__main__':
+    app.debug=True
+    app.run(host = '0.0.0.0', port=5000)
