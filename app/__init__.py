e7f316a2262c9ab62ebc282dd66811f556aa4577
bmugenya <bmugenya26@gmail.com>
2020-03-06 22:15:36 +0300

    [FEATURE #171675295] imported database

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,17 @@
-from flask import Flask,render_template,Blueprint
+import os
+from flask import Flask,Blueprint
 from flask_restful import Resource
 
 from .api.v1 import version_one as v1
+from .db_con import database_setup
+
+UPLOAD_FOLDER ='app/static/img'
+
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(v1)
+
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
     return app
