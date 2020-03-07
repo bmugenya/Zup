70f3987117e76ed36ead975bddefde44c2a407a2
bmugenya <bmugenya26@gmail.com>
2020-03-07 21:11:29 +0300

    [FEATURE #171678550] Included only routes

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,5 +1,6 @@
 from .model.model import Model
 from .view.view import *
+from .view.user import *
 
 
 from flask_restful import Api, Resource
@@ -13,12 +14,11 @@ api = Api(version_one)
 api.add_resource(index, "/")
 api.add_resource(contact, "/contact")
 api.add_resource(about, "/about")
-api.add_resource(dashboard, "/dashboard")
 api.add_resource(addUsers, "/adduser")
 api.add_resource(users, "/user")
 api.add_resource(config, "/config")
 api.add_resource(analysis, "/analysis")
-api.add_resource(details, "/details")
-api.add_resource(history, "/history")
-api.add_resource(report, "/report")
+api.add_resource(reports, "/reports")
+api.add_resource(report, "/report/<int:report_id>")
+api.add_resource(logout, "/logout")
 
