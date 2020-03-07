f22bb17f796a782299c1063e85bccf2b2c077471
bmugenya <bmugenya26@gmail.com>
2020-03-07 21:13:18 +0300

    [FEATURE #171678550] imported jwt

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_restful import Resource
 
 from .api.v1 import version_one as v1
 from .db_con import database_setup
+from flask_jwt_extended import JWTManager
 
 UPLOAD_FOLDER ='app/static/img'
 
@@ -13,5 +14,13 @@ def create_app():
     app.register_blueprint(v1)
 
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+    app.secret_key ="371a7a78d634dd71ce5f215d4827ea919d00ea50a9e20482d7adefd8a5156b78"
+
+    JWTManager(app)
+    jwt = JWTManager(app)
+    app.config['JWT_TOKEN_LOCATION'] = ['cookies']
+    app.config['JWT_ACCESS_COOKIE_PATH'] = '/'
+    app.config['JWT_COOKIE_CSRF_PROTECT'] = False
+
 
     return app
